05a1aa0c2cb7c9a66fb8a0b23b423689bbfeb211
leovigildoMiuel <leovigildomiguel@live.com.pt>
2026-02-11 18:38:45 -0300

    Atualização da migrations das extetions no banco de dados

---
+++ OnClickInvest.Api/Migrations/20260211213626_AddSharedEntities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnClickInvest.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSharedEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "audit_logs",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "audit_logs");
+        }
+    }
+}
